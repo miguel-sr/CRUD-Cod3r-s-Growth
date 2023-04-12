71e5a59da6a643aeed431100533095e0f5465b62
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-12 12:29:29 -0300

    :sparkles: feat: Mostra lista de peças disponíveis

--- Cod3rsGrowth/Form1.cs
+++ Cod3rsGrowth/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cod3rsGrowth.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,32 @@ namespace Cod3rsGrowth
 {
     public partial class Form1 : Form
     {
+        private DataGridView categoriesDataGridView = new DataGridView();
+
         public Form1()
         {
             InitializeComponent();
+            SetupDataGridView();
+        }
+
+        private void SetupDataGridView() 
+        {
+            this.Controls.Add(categoriesDataGridView);
+            categoriesDataGridView.Name = "Lista de Peças";
+            categoriesDataGridView.Size = new Size(600, 150);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Part> parts = new List<Part>();
+            parts.Add(new Part("1", "Parafusos", "Parafuso Sextavado", "Espessura 6mm", 30));
+            parts.Add(new Part("2", "Parafusos", "Parafuso Sextavado", "Espessura 8mm", 15));
+            parts.Add(new Part("3", "Porcas", "Porca Autotravante", "Espessura 6mm", 30));
+            parts.Add(new Part("4", "Porcas", "Porca Autotravante", "Espessura 8mm", 15));
+            categoriesDataGridView.DataSource = parts;
+
+
+
         }
     }
 }
