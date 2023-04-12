ad3ab7db92f163b3a2f9e50408df7e010a418dda
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-12 12:52:34 -0300

    :recycle: refactor: Alinhamento do conteúdo no centro das células

--- Cod3rsGrowth/Form1.cs
+++ Cod3rsGrowth/Form1.cs
@@ -30,11 +30,14 @@ namespace Cod3rsGrowth
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            List<Part> parts = new List<Part>();
-            parts.Add(new Part("1", "Parafusos", "Parafuso Sextavado", "Espessura 6mm", 30));
-            parts.Add(new Part("2", "Parafusos", "Parafuso Sextavado", "Espessura 8mm", 15));
-            parts.Add(new Part("3", "Porcas", "Porca Autotravante", "Espessura 6mm", 30));
-            parts.Add(new Part("4", "Porcas", "Porca Autotravante", "Espessura 8mm", 15));
+            List<Part> parts = new List<Part>
+            {
+                new Part("1", "Parafusos", "Parafuso Sextavado", "Espessura 6mm", 30),
+                new Part("2", "Parafusos", "Parafuso Sextavado", "Espessura 8mm", 15),
+                new Part("3", "Porcas", "Porca Autotravante", "Espessura 6mm", 30),
+                new Part("4", "Porcas", "Porca Autotravante", "Espessura 8mm", 15)
+            };
+
             categoriesDataGridView.DataSource = parts;
 
             categoriesDataGridView.Columns[0].HeaderText = "#";
@@ -42,6 +45,13 @@ namespace Cod3rsGrowth
             categoriesDataGridView.Columns[2].HeaderText = "Nome";
             categoriesDataGridView.Columns[3].HeaderText = "Descrição";
             categoriesDataGridView.Columns[4].HeaderText = "Estoque";
+
+            for (int i = 0; i < 5; i++)
+            {
+                categoriesDataGridView.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+
+            categoriesDataGridView.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
         }
     }
 }
