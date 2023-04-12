67fa2d6dcfc52d4036b9b9a24cf4f63741dfcc60
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-12 11:13:02 -0300

    :sparkles: feat: Adiciona modelos dos objetos

---
+++ Cod3rsGrowth/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Models
+{
+    public class Category
+    {
+
+        public Category(string id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public string id { get; set; }
+
+        public string name { get; set; }
+    }
+}
