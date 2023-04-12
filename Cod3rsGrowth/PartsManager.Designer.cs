09f0339c271f011d91e7396eada5712a1119b494
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-04-12 20:45:37 -0300

    :recycle: refactor: Altera nomenclatura dos botões

--- Cod3rsGrowth/PartsManager.Designer.cs
+++ Cod3rsGrowth/PartsManager.Designer.cs
@@ -28,16 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.ButtonToRemovePart = new System.Windows.Forms.Button();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ButtonToAddNewPart = new System.Windows.Forms.Button();
+            this.ButtonToUpdatePart = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,46 +50,29 @@
             this.Column3,
             this.Column4,
             this.Column5});
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(776, 342);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // button1
+            // ButtonToRemovePart
             // 
-            this.button1.Location = new System.Drawing.Point(185, 401);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 37);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Adicionar";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(329, 401);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 37);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Editar";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(473, 401);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(138, 37);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Remover";
-            this.button3.UseVisualStyleBackColor = true;
+            this.ButtonToRemovePart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonToRemovePart.Location = new System.Drawing.Point(506, 401);
+            this.ButtonToRemovePart.Name = "ButtonToRemovePart";
+            this.ButtonToRemovePart.Size = new System.Drawing.Size(138, 37);
+            this.ButtonToRemovePart.TabIndex = 3;
+            this.ButtonToRemovePart.Text = "Remover";
+            this.ButtonToRemovePart.UseVisualStyleBackColor = true;
             // 
             // Column1
             // 
@@ -132,14 +115,34 @@
             this.Column5.Name = "Column5";
             this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
+            // ButtonToAddNewPart
+            // 
+            this.ButtonToAddNewPart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonToAddNewPart.Location = new System.Drawing.Point(156, 401);
+            this.ButtonToAddNewPart.Name = "ButtonToAddNewPart";
+            this.ButtonToAddNewPart.Size = new System.Drawing.Size(138, 37);
+            this.ButtonToAddNewPart.TabIndex = 4;
+            this.ButtonToAddNewPart.Text = "Adicionar";
+            this.ButtonToAddNewPart.UseVisualStyleBackColor = true;
+            // 
+            // ButtonToUpdatePart
+            // 
+            this.ButtonToUpdatePart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonToUpdatePart.Location = new System.Drawing.Point(331, 401);
+            this.ButtonToUpdatePart.Name = "ButtonToUpdatePart";
+            this.ButtonToUpdatePart.Size = new System.Drawing.Size(138, 37);
+            this.ButtonToUpdatePart.TabIndex = 5;
+            this.ButtonToUpdatePart.Text = "Editar";
+            this.ButtonToUpdatePart.UseVisualStyleBackColor = true;
+            // 
             // PartsManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonToUpdatePart);
+            this.Controls.Add(this.ButtonToAddNewPart);
+            this.Controls.Add(this.ButtonToRemovePart);
             this.Controls.Add(this.dataGridView1);
             this.Name = "PartsManager";
             this.Text = "Controle de Peças";
@@ -152,14 +155,14 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button ButtonToRemovePart;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Button ButtonToAddNewPart;
+        private System.Windows.Forms.Button ButtonToUpdatePart;
     }
 }
 
