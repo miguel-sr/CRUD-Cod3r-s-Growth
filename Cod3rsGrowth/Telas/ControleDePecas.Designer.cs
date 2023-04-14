ff5e4376ff05de2b573ff7a8b37fb243b64b63f4
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-14 07:56:48 -0300

    :recycle: refactor: Trocando referência dos botões

--- Cod3rsGrowth/Telas/ControleDePecas.Designer.cs
+++ Cod3rsGrowth/Telas/ControleDePecas.Designer.cs
@@ -38,9 +38,6 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
             this.GridDePecas = new System.Windows.Forms.DataGridView();
-            this.aoClicarRemoverPecaSelecionada = new System.Windows.Forms.Button();
-            this.aoClicarTrocarParaMenuDeCriarPeca = new System.Windows.Forms.Button();
-            this.aoClicarAbrirMenuDeEdicaoDePeca = new System.Windows.Forms.Button();
             this.ColunaId = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColunaCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColunaNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -48,6 +45,9 @@
             this.ColunaEstoque = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColunaDataDeFabricacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.pecaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.AoClicarRemoverPecaSelecionada = new System.Windows.Forms.Button();
+            this.AoClicarTrocarParaMenuDeCriarPeca = new System.Windows.Forms.Button();
+            this.AoClicarAbrirMenuDeEdicaoDePeca = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.GridDePecas)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pecaBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -84,37 +84,6 @@
             this.GridDePecas.Name = "GridDePecas";
             this.GridDePecas.Size = new System.Drawing.Size(796, 347);
             this.GridDePecas.TabIndex = 0;
-            // 
-            // aoClicarRemoverPecaSelecionada
-            // 
-            this.aoClicarRemoverPecaSelecionada.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.aoClicarRemoverPecaSelecionada.Location = new System.Drawing.Point(670, 376);
-            this.aoClicarRemoverPecaSelecionada.Name = "aoClicarRemoverPecaSelecionada";
-            this.aoClicarRemoverPecaSelecionada.Size = new System.Drawing.Size(138, 37);
-            this.aoClicarRemoverPecaSelecionada.TabIndex = 3;
-            this.aoClicarRemoverPecaSelecionada.Text = "Remover";
-            this.aoClicarRemoverPecaSelecionada.UseVisualStyleBackColor = true;
-            // 
-            // aoClicarTrocarParaMenuDeCriarPeca
-            // 
-            this.aoClicarTrocarParaMenuDeCriarPeca.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.aoClicarTrocarParaMenuDeCriarPeca.Location = new System.Drawing.Point(382, 376);
-            this.aoClicarTrocarParaMenuDeCriarPeca.Name = "aoClicarTrocarParaMenuDeCriarPeca";
-            this.aoClicarTrocarParaMenuDeCriarPeca.Size = new System.Drawing.Size(138, 37);
-            this.aoClicarTrocarParaMenuDeCriarPeca.TabIndex = 4;
-            this.aoClicarTrocarParaMenuDeCriarPeca.Text = "Adicionar";
-            this.aoClicarTrocarParaMenuDeCriarPeca.UseVisualStyleBackColor = true;
-            this.aoClicarTrocarParaMenuDeCriarPeca.Click += new System.EventHandler(this.AoClicarTrocarParaMenuDeCriarPeca_Click);
-            // 
-            // aoClicarAbrirMenuDeEdicaoDePeca
-            // 
-            this.aoClicarAbrirMenuDeEdicaoDePeca.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.aoClicarAbrirMenuDeEdicaoDePeca.Location = new System.Drawing.Point(526, 376);
-            this.aoClicarAbrirMenuDeEdicaoDePeca.Name = "aoClicarAbrirMenuDeEdicaoDePeca";
-            this.aoClicarAbrirMenuDeEdicaoDePeca.Size = new System.Drawing.Size(138, 37);
-            this.aoClicarAbrirMenuDeEdicaoDePeca.TabIndex = 5;
-            this.aoClicarAbrirMenuDeEdicaoDePeca.Text = "Editar";
-            this.aoClicarAbrirMenuDeEdicaoDePeca.UseVisualStyleBackColor = true;
             // 
             // ColunaId
             // 
@@ -176,15 +145,46 @@
             // 
             this.pecaBindingSource.DataSource = typeof(Cod3rsGrowth.Modelos.Peca);
             // 
+            // AoClicarRemoverPecaSelecionada
+            // 
+            this.AoClicarRemoverPecaSelecionada.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AoClicarRemoverPecaSelecionada.Location = new System.Drawing.Point(670, 376);
+            this.AoClicarRemoverPecaSelecionada.Name = "AoClicarRemoverPecaSelecionada";
+            this.AoClicarRemoverPecaSelecionada.Size = new System.Drawing.Size(138, 37);
+            this.AoClicarRemoverPecaSelecionada.TabIndex = 3;
+            this.AoClicarRemoverPecaSelecionada.Text = "Remover";
+            this.AoClicarRemoverPecaSelecionada.UseVisualStyleBackColor = true;
+            // 
+            // AoClicarTrocarParaMenuDeCriarPeca
+            // 
+            this.AoClicarTrocarParaMenuDeCriarPeca.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AoClicarTrocarParaMenuDeCriarPeca.Location = new System.Drawing.Point(382, 376);
+            this.AoClicarTrocarParaMenuDeCriarPeca.Name = "AoClicarTrocarParaMenuDeCriarPeca";
+            this.AoClicarTrocarParaMenuDeCriarPeca.Size = new System.Drawing.Size(138, 37);
+            this.AoClicarTrocarParaMenuDeCriarPeca.TabIndex = 4;
+            this.AoClicarTrocarParaMenuDeCriarPeca.Text = "Adicionar";
+            this.AoClicarTrocarParaMenuDeCriarPeca.UseVisualStyleBackColor = true;
+            this.AoClicarTrocarParaMenuDeCriarPeca.Click += new System.EventHandler(this.AoClicarTrocarParaMenuDeCriarPeca_Click);
+            // 
+            // AoClicarAbrirMenuDeEdicaoDePeca
+            // 
+            this.AoClicarAbrirMenuDeEdicaoDePeca.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AoClicarAbrirMenuDeEdicaoDePeca.Location = new System.Drawing.Point(526, 376);
+            this.AoClicarAbrirMenuDeEdicaoDePeca.Name = "AoClicarAbrirMenuDeEdicaoDePeca";
+            this.AoClicarAbrirMenuDeEdicaoDePeca.Size = new System.Drawing.Size(138, 37);
+            this.AoClicarAbrirMenuDeEdicaoDePeca.TabIndex = 5;
+            this.AoClicarAbrirMenuDeEdicaoDePeca.Text = "Editar";
+            this.AoClicarAbrirMenuDeEdicaoDePeca.UseVisualStyleBackColor = true;
+            // 
             // ControleDePecas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(825, 435);
-            this.Controls.Add(this.aoClicarRemoverPecaSelecionada);
-            this.Controls.Add(this.aoClicarAbrirMenuDeEdicaoDePeca);
+            this.Controls.Add(this.AoClicarRemoverPecaSelecionada);
+            this.Controls.Add(this.AoClicarAbrirMenuDeEdicaoDePeca);
             this.Controls.Add(this.GridDePecas);
-            this.Controls.Add(this.aoClicarTrocarParaMenuDeCriarPeca);
+            this.Controls.Add(this.AoClicarTrocarParaMenuDeCriarPeca);
             this.Name = "ControleDePecas";
             this.Text = "Controle de Peças";
             this.Load += new System.EventHandler(this.ControleDePecas_Load);
@@ -195,9 +195,9 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button aoClicarRemoverPecaSelecionada;
-        private System.Windows.Forms.Button aoClicarTrocarParaMenuDeCriarPeca;
-        private System.Windows.Forms.Button aoClicarAbrirMenuDeEdicaoDePeca;
+        private System.Windows.Forms.Button AoClicarRemoverPecaSelecionada;
+        private System.Windows.Forms.Button AoClicarTrocarParaMenuDeCriarPeca;
+        private System.Windows.Forms.Button AoClicarAbrirMenuDeEdicaoDePeca;
         private System.Windows.Forms.DataGridView GridDePecas;
         private System.Windows.Forms.BindingSource pecaBindingSource;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColunaId;
