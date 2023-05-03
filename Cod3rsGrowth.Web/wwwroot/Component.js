67e037ee44f45e25a598450709c1b68c1672e968
Miguel S Ramos <miguelsramos458@gmail.com>
2023-05-03 10:36:52 -0300

    :sparkles: feat: Cria setup para interface web usando SAPUI5

---
+++ Cod3rsGrowth.Web/wwwroot/Component.js
@@ -0,0 +1,16 @@
+﻿sap.ui.define(
+    ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel", "sap/ui/Device"],
+    function (UIComponent) {
+        "use strict";
+
+        return UIComponent.extend("sap.ui.demo.Cod3rsGrowth.Component", {
+            metadata: {
+                interfaces: ["sap.ui.core.IAsyncContentCreation"],
+                manifest: "json",
+            },
+            init: function () {
+                UIComponent.prototype.init.apply(this, arguments);
+            }
+        });
+    }
+);
---
+++ Cod3rsGrowth.Web/wwwroot/controller/App.controller.js
@@ -0,0 +1,9 @@
+﻿sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.Cod3rsGrowth.controller.App", {
+        onShowHello: function () {
+            alert("Hello World");
+        },
+    });
+});
