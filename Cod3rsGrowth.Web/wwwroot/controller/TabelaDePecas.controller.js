e347f59e3ac8f3391b7259b3ff4cb17cb7469dc3
Miguel S Ramos <miguelsramos458@gmail.com>
2023-05-08 10:48:14 -0300

    :goal_net: Adiciona try/catch no request

--- Cod3rsGrowth.Web/wwwroot/controller/TabelaDePecas.controller.js
+++ Cod3rsGrowth.Web/wwwroot/controller/TabelaDePecas.controller.js
@@ -4,25 +4,31 @@ sap.ui.define(
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
+    "sap/m/MessageToast",
   ],
   /**
    * @param {typeof import('sap/ui/core/mvc/Controller').default} Controller
    * @param {typeof import('sap/ui/model/json/JSONModel').default} JSONModel
    * @param {typeof import('sap/ui/model/Filter').default} Filter
    * @param {typeof import('sap/ui/model/FilterOperator').default} FilterOperator
+   *  @param {typeof import('sap/m/MessageToast').default} Dialog
    */
-  function (Controller, JSONModel, Filter, FilterOperator) {
+  function (Controller, JSONModel, Filter, FilterOperator, MessageToast) {
     "use strict";
 
     return Controller.extend("sap.ui.cod3rsgrowth.controller.TabelaDePecas", {
       onInit: async function () {
-        var oModel = new JSONModel();
+        try {
+          var oModel = new JSONModel();
 
-        var pecas = await (await fetch("http://localhost:5285/pecas")).json();
+          var pecas = await (await fetch("http://localhost:5285/pecas")).json();
 
-        oModel.setData({ pecas });
+          oModel.setData({ pecas });
 
-        this.getView().setModel(oModel);
+          this.getView().setModel(oModel);
+        } catch (error) {
+          MessageToast.show("Erro ao obter itens da tabela.");
+        }
       },
 
       aoClicarMostrarDetalhes: async function (oEvent) {},
